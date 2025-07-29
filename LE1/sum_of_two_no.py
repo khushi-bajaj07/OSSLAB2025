d37bcd5475066505848cac91452ec0cf3a8f842e
231B419 <231B419@juet.co.in>
2025-07-29 12:49:24 +0530

    ADDED SOLUTIOPN TO LE1

---
+++ LE1/sum_of_two_no.py
@@ -0,0 +1,6 @@
+Python 3.10.11 (tags/v3.10.11:7d4cc5a, Apr  5 2023, 00:38:17) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> num1 = float(input("Enter first number: "))
+... num2 = float(input("Enter second number: "))
+... sum = num1 + num2
+... print("The sum is:", sum)
